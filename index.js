6745412c034524d9685f3e3a41ce6275bd350dcb
IMACULGY <drewisaacson@live.com>
2020-09-29 12:08:41 -0400

    I feel calm but energized

--- index.js
+++ index.js
@@ -2,21 +2,15 @@
  * A ping-pong bot, whenever you send "!ping", it replies "pong!".
  */
 
-// Import the discord.js module
 const Discord = require("discord.js");
 // Create an instance of a Discord client
 const client = new Discord.Client();
 const config = require("./config.json");
  
-/**
- * The ready event is vital, it means that only _after_ this will your bot
- * start reacting to information received from Discord
- */
 client.on("ready", () => {
     console.log("I am ready!");
 });
  
-// Create an event listener for messages
 client.on("message", (message) => {
     // Ignore the message if the author of the message is another bot.
     if(message.author.bot) return;
@@ -24,25 +18,22 @@ client.on("message", (message) => {
     // Ignore the message if the message does not start with the specified prefix.
     if(message.content.indexOf(config.prefix) != 0) return;
     
-    /**
-     * Here we separate our "command" name, and our "arguments" for the command. 
-     * e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-     * command = say
-     * args = ["Is", "this", "the", "real", "life?"]
-    */
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    // If the command is "ping", send "pong!" to the same channel or DM.
-    // If the command is "bada-bing", send "bada-boom!"
-    // If the command is "creeper", send "aww man"
-    if (command === "ping") {
-        message.channel.send("pong!");
-    } else if (command === "bada-bing") {
-        message.channel.send("bada-boom!");
-    } else if (command === "creeper") {
-        message.channel.send("aww man");
-    }
+    // NUMBER OF COMMANDS: 3
+	switch(command) {
+		case "ping":
+			message.channel.send("pong!");
+			break;
+		case "bada-bing"
+			message.channel.send("bada-boom")
+			break;
+		case "creeper"
+			message.channel.send("aww man");
+			break;
+	}
+	
 });
  
 // Log the bot in using the token from https://discordapp.com/developers/applications/me
